ce1c7b7b9b5ff9084b4d1c99fc62f08d2e6bfb64
Kyle Lazera <klazera@gmail.com>
2024-10-06 10:42:17 -0400

    Fixed bug in TXMAC

--- rtl/Ethernet/tx_mac.sv
+++ rtl/Ethernet/tx_mac.sv
@@ -13,7 +13,7 @@
 
 /*
  * TODO: 
- * 1) Handle and Drive the rgmii_mac_tx_dv & rgmii_mac_tx_er signals
+ * 1) Handle the rgmii_mac_tx_er signals
  * 2) IFG length for different link throughputs (10/100/1000 Mbps)
 */
 
@@ -64,11 +64,14 @@ typedef enum{IDLE,              //State when no transactions are occuring
 /* Signal Declarations */
 state_type state_reg, state_next;                               //Holds the current and next State 
 reg [DATA_WIDTH-1:0] tx_data_reg, tx_data_next;                 //Holds data to be transmitted to RGMII
+reg rgmii_dv_reg, rgmii_dv_next;                                //Data Valid signal for the RGMII
+reg rgmii_er_reg, rgmii_er_next;                                //Error Signal for teh RGMII
 reg [2:0] byte_ctr, byte_ctr_next;                              //Counts the number of bytes transmitted
 reg [7:0] pckt_size, pckt_size_next;                            //Counts size of payload in bytes (Ensure they payload is between 46 - 1500 bytes) 
 reg mii_sdr, mii_sdr_next;                                      //Indicates the next data transfer needs to shift byte by 4 (for SDR in MII mode)
 reg axis_rdy_reg, axis_rdy_next;                                //Implements a FF between the outgoing s_tx_axis_rdy signal and FIFO  
 reg [3:0] ifg_ctr, ifg_ctr_next;
+
 
 /* CRC32 Interface Signals */
 reg [31:0] crc_state, crc_next;                                 //Holds the output state of the CRC32 module                                       
@@ -76,8 +79,18 @@ reg [DATA_WIDTH-1:0] crc_in_data_reg, crc_in_data_next;         //Holds the inpu
 reg crc_en_reg, crc_en_next;                                    //Register that holds crc_en state 
 wire [DATA_WIDTH-1:0] crc_data_in;                              //Signal that drives the CRC data into the module
 reg sof;                                                        //Start of frame signal                              
-wire crc_en;                                                    //CRC enable - this enables the CRC32 module to operate  
-wire [31:0] crc_data_out;    
+wire crc_en, crc_reset;                                         //CRC enable & reset   
+wire [31:0] crc_data_out;                                       //Ouput from the CRC32 module
+
+/* CRC32 Module Instantiation */
+crc32 #(.DATA_WIDTH(8)) 
+crc_module(.clk(clk),
+           .i_byte(crc_data_in),
+           .i_crc_state(crc_state),
+           .crc_en(crc_en),
+           .o_crc_state(crc_next),
+           .crc_out(crc_data_out)
+           );
 
 /* Sequential Logic */
 always @(posedge clk) begin
@@ -91,20 +104,25 @@ always @(posedge clk) begin
         crc_en_reg <= 1'b0;
         crc_in_data_reg <= 1'b0;
         ifg_ctr <= 1'b0;
-        crc_state <= 32'hFFFFFFFF;
+        rgmii_dv_reg <= 1'b0;
+        rgmii_er_reg <= 1'b0;
     end else begin
         state_reg <= state_next;
         tx_data_reg <= tx_data_next;
+        rgmii_dv_reg <= rgmii_dv_next;
+        rgmii_er_reg <= rgmii_er_next;
         byte_ctr <= byte_ctr_next;
         pckt_size <= pckt_size_next;
         mii_sdr <= mii_sdr_next;
         axis_rdy_reg <= axis_rdy_next;
-        crc_en_reg <= crc_en_next;
-        crc_in_data_reg <= crc_in_data_next;
         ifg_ctr <= ifg_ctr_next;
+                
+        /* CRC Data Updates */
+        crc_en_reg <= crc_en_next;
+        crc_in_data_reg <= crc_in_data_next;        
         
         /* Logic to update CRC State */
-        if(sof)
+        if(crc_reset)
             crc_state <= 32'hFFFFFFFF;
         else if(crc_en)
             crc_state <= crc_next;
@@ -117,6 +135,7 @@ end
 
 assign crc_en = crc_en_reg;
 assign crc_data_in = crc_in_data_reg;
+assign crc_reset = (~reset_n || sof);
 
 /* Next State Logic */
 always @(*) begin
@@ -130,6 +149,9 @@ always @(*) begin
     mii_sdr_next = 1'b1;
     axis_rdy_next = 1'b0;
     crc_en_next = 1'b0;
+    rgmii_dv_next = 1'b0;
+    rgmii_er_next = 1'b0;
+    sof = 1'b0;
  
     //If the RGMII is NOT ready to recieve data - pause the FSM operation
     if(!rgmii_mac_tx_rdy) begin
@@ -148,15 +170,18 @@ always @(*) begin
         /* FSM Next State Logic */
         case(state_reg) 
             IDLE : begin
-                //If there is data in the FIFO, prepare to being transaction
+                //If there is data in the FIFO, prepare to begin transaction
                 if(s_tx_axis_tvalid) begin
+                    sof = 1'b1;
                     byte_ctr_next = 3'b0;
                     mii_sdr_next = 1'b0;
                     pckt_size_next = 6'b0;
+                    rgmii_dv_next = 1'b1;
                     state_next = PREAMBLE;
                 end
             end
             PREAMBLE : begin
+                rgmii_dv_next = 1'b1;
                 //Set the s_axis_trdy flag high here, so we have incoming data when we enter the PACKET State
                 if(byte_ctr == 3'd6) begin
                     axis_rdy_next = 1'b1; 
@@ -178,12 +203,18 @@ always @(*) begin
                 end
             end
             PACKET : begin
+                rgmii_dv_next = 1'b1;
                 crc_en_next = 1'b1;
                 crc_in_data_next = s_tx_axis_tdata;
                 axis_rdy_next = 1'b1;
                 tx_data_next = s_tx_axis_tdata;
                 mii_sdr_next = 1'b1;
-                pckt_size_next = pckt_size + 1;
+                
+                //Only increment the packet counter if it is less than 60. Once min frame size has been surpassed
+                //packet counter is no longer needed
+                //TODO: Better way to implement this?? Maybe ussing a continous assignment to set a flag in this case
+                if(pckt_size < (MIN_FRAME_WIDTH + 1))
+                    pckt_size_next = pckt_size + 1;
                 
                 //If the last beat has arrived OR there is no more valid data in the FIFO
                 //TODO: Possibly deal with error flag here for RGMII
@@ -196,7 +227,8 @@ always @(*) begin
                     end
                 end
             end
-            PADDING : begin            
+            PADDING : begin  
+                rgmii_dv_next = 1'b1;          
                 crc_in_data_next = ETH_PAD;
                 tx_data_next = ETH_PAD;
                 pckt_size_next = pckt_size + 1;                               
@@ -211,6 +243,7 @@ always @(*) begin
                 end 
             end            
             FCS : begin
+                rgmii_dv_next = 1'b1;
                 //Multiplex to determine which bytes to transmit
                 case (byte_ctr)
                     3'b11 : tx_data_next = crc_data_out[7:0];
@@ -227,6 +260,8 @@ always @(*) begin
                 end else
                     byte_ctr_next = byte_ctr - 1;
             end
+            //According to IEEE, the required IFG bewteen packets is 96 bit-times
+            //For 1Gbit this is 96ns, for 100Mbps this is 0.96us and 10Mbps is 9.6us
             IFG : begin
                 if(ifg_ctr > 4'd12) 
                     state_next = IDLE;
@@ -236,19 +271,10 @@ always @(*) begin
         endcase
     end
 end
-
-/* CRC32 Module Instantiation */
-crc32 #(.DATA_WIDTH(8)) 
-crc_module(.clk(clk),
-           .i_byte(crc_data_in),
-           .i_crc_state(crc_state),
-           .crc_en(crc_en),
-           .o_crc_state(crc_next),
-           .crc_out(crc_data_out)
-           );
            
 /* Output Logic */         
 assign rgmii_mac_tx_data = tx_data_reg;
 assign s_tx_axis_trdy = axis_rdy_reg;
+assign rgmii_mac_tx_dv = rgmii_dv_reg;
 
 endmodule
