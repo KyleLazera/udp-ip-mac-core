8a635a0c7b87ba2b5ea4f490eb245e013afb114d
Kyle Lazera <klazera@gmail.com>
2024-10-12 10:17:04 -0400

    State Machine for tb

--- sim/Ethernet_MAC/tax_mac_monitor.sv
+++ sim/Ethernet_MAC/tax_mac_monitor.sv
@@ -4,34 +4,100 @@
 `include "tx_mac_trans_item.sv"
 
 class tx_mac_monitor;
-    //Mailbox for scoreboard communication
-    mailbox scb_mbx;
-    //Virtual interface
-    virtual tx_mac_if vif;
-    //Tag for debugging/printing
-    string TAG = "Monitor";
+    /* Class variables */
+    mailbox scb_mbx;                //Mailbox for scoreboard communication
+    virtual tx_mac_if vif;          //Virtual interface
+    string TAG = "Monitor";         //Tag for debugging/printing
     
+    typedef enum {IDLE,             //Initial waiting state
+                  PREAMBLE,         //Check the preamble
+                  PAYLOAD,          //Check the payload
+                  CRC,              //Check teh CRC/FCS
+                  IFG               //IFG before next packet
+                  }state_type;
+                     
     //Constructor
     function new(mailbox _mbx);
         scb_mbx = _mbx;
     endfunction : new  
     
     task main();
+        /* Monitor Variables */
         tx_mac_trans_item rec_item = new;
-        bit pckt_synch = 1'b0;
-        $display("[%s] Starting...", TAG);
+
+        state_type state = IDLE;
+        int byte_ctr = 0;
+        bit last_byte = 1'b0;
         
-        @(posedge vif.clk);
+        logic [7:0] ethernet_pckt[$];
+        //Queue to hold the preamble/SFD
+        logic [7:0] preamble [$];         
+        
+        $display("[%s] Starting...", TAG);
         
         forever begin
             //Sample the data being transmitted to the RGMII on every clock pulse
-            @(posedge vif.clk);
-            rec_item.data_byte = vif.rgmii_mac_tx_data;        
-            //Transmit this data to the scoreboard
-            scb_mbx.put(rec_item);
+            @(posedge vif.clk);  
+            
+            case(state)
+                IDLE : begin
+                    //If there is valid data in the FIFO go to the preamble state
+                    if(vif.s_tx_axis_tvalid)
+                        state = PREAMBLE;
+                end
+                PREAMBLE : begin
+                    if(byte_ctr < 8) begin
+                        #1 rec_item.preamble[byte_ctr] = vif.rgmii_mac_tx_data;
+                        byte_ctr++;
+                    end else begin
+                        byte_ctr = 0;
+                        state = PAYLOAD;
+                    end 
+                                                                                       
+                end
+                PAYLOAD : begin                                    
+                    #1 rec_item.payload.push_back(vif.rgmii_mac_tx_data);
+                    
+                    if(last_byte) begin
+                        last_byte = 1'b0;
+                        state = CRC;
+                        byte_ctr = 0;
+                    end else begin                                                
+                        if(vif.s_tx_axis_tlast) begin
+                            last_byte = 1'b1;
+                        end
+                    end
+                end
+                CRC : begin
+                    //Populate the CRC bytes after small delay
+                    #1;
+                    case(byte_ctr)
+                        0 : rec_item.fcs[0] = vif.rgmii_mac_tx_data;
+                        1 : rec_item.fcs[1] = vif.rgmii_mac_tx_data;
+                        2 : rec_item.fcs[2] = vif.rgmii_mac_tx_data;
+                        3 : rec_item.fcs[3] = vif.rgmii_mac_tx_data;
+                    endcase
+                   
+                    if(byte_ctr == 3) begin
+                        state = IFG;
+                        byte_ctr = 0;
+                    end else
+                        byte_ctr++;
+                   
+                end
+                IFG : begin
+                //Wait for the IFG and send transaction item to scoreboard
+                if(byte_ctr > 4'd12) begin 
+                    state = IDLE;
+                    scb_mbx.put(rec_item);                    
+                end else 
+                    byte_ctr++;
+                end
+            endcase                             
         end
         
     endtask : main 
+  
     
 endclass : tx_mac_monitor
 
--- sim/Ethernet_MAC/tx_mac_trans_item.sv
+++ sim/Ethernet_MAC/tx_mac_trans_item.sv
@@ -8,6 +8,12 @@ class tx_mac_trans_item;
     rand logic [7:0] data_byte;
     rand int pckt_size;
     
+    /* Variables to transmit from monitor to scb */
+    logic [7:0] preamble [7:0];             //8 bytes of preamble
+    logic [7:0] payload [$];                //payload size dependent upon pckt_size
+    logic [7:0] fcs [3:0];                  //4 bytes of FCS
+    
+    
     //Constrains the number of bytes within a packet, the calculation for these numbers:
     //64 bytes minimum including src addr, dst addr, type/length, payload and 4 bytes of CRC
     //Remove the CRC as this will be generated, we only want to simulate Addresses, Type/Length & Payload 
